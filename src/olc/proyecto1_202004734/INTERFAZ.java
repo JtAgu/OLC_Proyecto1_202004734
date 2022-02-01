96b2f37bc2df25a532b30666046146c30ffd71f0
Justin Aguirre <justinaguirre59@gmail.com>
2022-02-01 09:06:42 -0600

    creacion

---
+++ src/olc/proyecto1_202004734/INTERFAZ.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc.proyecto1_202004734;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+/**
+ *
+ * @author justin
+ */
+public class INTERFAZ {
+    
+}
